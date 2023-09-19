32ee244c705d354c83728baf00ab454ca419bd26
大濵彰博 <m14t8002m@gmail.com>
2023-09-19 22:07:49 +0900

    Java-基本文法課題01 add

---
+++ src/main/java/static_project/PracticeWhile.java
@@ -0,0 +1,12 @@
+package static_project;
+
+public class PracticeWhile {
+	public static void main(String[] args) {
+		int numberCount = 100; // 初期値
+
+		while (numberCount >= 0) { //0未満までループ
+			System.out.println(numberCount);
+			numberCount--;
+		}
+	}
+}
